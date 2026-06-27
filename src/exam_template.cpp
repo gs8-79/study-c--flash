c1f9650fcc7f3feb89f05595bbee13607127fa49
gs8-79 <1706463402@qq.com>
2026-06-27 21:33:20 +0800

    Add exam template

---
+++ src/exam_template.cpp
@@ -0,0 +1,21 @@
+#include <algorithm>
+#include <cmath>
+#include <fstream>
+#include <iomanip>
+#include <iostream>
+#include <stdexcept>
+#include <string>
+#include <vector>
+using namespace std;
+
+// 1. Write classes here.
+
+// 2. Write helper functions here.
+
+int main() {
+    // 3. Read input or prepare test data here.
+
+    // 4. Call functions and print result here.
+
+    return 0;
+}
